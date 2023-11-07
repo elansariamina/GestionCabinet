66d998e421a2831db4d3b330cbc1c307b7a14580
Amina EL ANSARI <aminaelansari21@gmail.com>
2023-11-07 15:31:11 +0100

    medecins page

--- cabinet/src/pages/Medecins.js
+++ cabinet/src/pages/Medecins.js
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
-import { MEDECINS } from '../components/ListMedecins'
-import MedecinCard from '../components/MedecinCard'
+import MedecinCard from '../components/MedecinCard';
+import axios from 'axios';
+
 
 function Medecins() {
     const { name } = useParams();
+    const [doctorsData, setDoctorsData] = useState([]);
+
+    useEffect(() => {
+      axios.get("http://localhost:3000/api/doctors")
+      .then(response => {
+          setDoctorsData(response.data);
+          console.log(response.data);
+      })
+      .catch(error => {
+        console.error("Error: ", error);
+      });
+    }, [])
   return (
     <div className='flex flex-wrap my-40'>
-    {MEDECINS.map((medecin) => (
-
-        medecin.speciality === name && <a href={`/patients/${medecin.id}`} key={medecin.id}>
+    {doctorsData.map((medecin) => (
+        medecin.spe === name && <a href={`/patients/${medecin.id}`} key={medecin.id}>
             <div className='flex justify-center p-2'>
             <MedecinCard doctor={medecin}/>
             </div> 
-            
         </a>
     ))}
 
