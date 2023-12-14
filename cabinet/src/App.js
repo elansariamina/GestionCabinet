2ba096b5900ffbe1af0286296eacc97427ac41ba
Amina EL ANSARI <aminaelansari21@gmail.com>
2023-12-14 14:47:56 +0100

    add rapports

--- cabinet/src/App.js
+++ cabinet/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Patients from './pages/Patient/Patients';
 import Medecins from './pages/Patient/Medecins';
 import Appointment from "./pages/Patient/Appointment";
+import AssociatedRapports from './pages/Patient/AssociatedRapports';
+import Rapports from './pages/Patient/Rapports';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
           {/*<Route path="/patients/:id" element={<Medecins />} />*/}
           <Route path="/appointment" element={<Appointment />} />
           <Route path="/patients/:name" element={<Medecins />} />
+          <Route path='/rapports/' element={<AssociatedRapports/>} />
         </Routes>
       </Router>
     </div>
