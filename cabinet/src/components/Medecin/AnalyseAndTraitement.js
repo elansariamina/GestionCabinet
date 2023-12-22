9f5a1a63630bd63c10f53c03fd404053cbe22630
kao19 <feiyukine@gmail.com>
2023-12-22 19:21:22 +0100

    storing analyes and traitements

--- cabinet/src/components/Medecin/AnalyseAndTraitement.js
+++ cabinet/src/components/Medecin/AnalyseAndTraitement.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import ListMedecaments from './ListMedecaments';
 import ListAnalyses from './ListAnalyses';
 import jsPDF from 'jspdf';
+import axios from 'axios';
 
-
-function AnalyseAndTraitement() {
+function AnalyseAndTraitement({ patientId, accessToken, doctorId }) {
     const [checkedOptions, setCheckedOptions] = useState([]);
     const [selectedOptions, setSelectedOptions] = useState([
         {
@@ -71,14 +71,42 @@ function AnalyseAndTraitement() {
             }
         });
       };
-      const generateAnalysePDF = () => {
+
+      const generateAnalysePDF = async () => {
         const pdf = new jsPDF();
         pdf.text('Checked Options', 10, 10);
-        checkedOptions.forEach((option, index)=>{
-            pdf.text(` - ${option} `, 20, 20 + (10 * index));
+        checkedOptions.forEach((option, index) => {
+          pdf.text(` - ${option} `, 20, 20 + (10 * index));
         });
-        pdf.save('analyses.pdf');
-      }
+        
+//        pdf.save('analyses.pdf');
+
+const pdfData = pdf.output('arraybuffer');
+const blob = new Blob([pdfData], { type: 'application/pdf' });
+
+const formData = new FormData();
+formData.append('patientId', patientId);
+formData.append('doctorId', doctorId);
+formData.append('type', 'analyse');
+formData.append('file', blob, 'analyses.pdf');
+
+try {
+  const response = await axios.post('http://localhost:3001/api/doctors/storeMedicalDoc', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+
+  console.log('Response:', response.data);
+
+        } catch (error) {
+          console.error('Error storing PDF on the server:', error);
+        }
+        
+      };
+      
+
       const generateOrdonancePDF = () => {
         const pdf = new jsPDF();
         pdf.text(`Ordonance`, 10,10);
@@ -91,8 +119,9 @@ function AnalyseAndTraitement() {
   return (
     <>
       <div>
-        <h1 className="text-xl font-bold mt-8 ml-12">Analyse</h1>
+        
         <div className='m-8 p-4 rounded-lg' style={{ boxShadow: '2px 2px 2px 2px rgba(0, 0, 0, 0.3)' }}>
+          <h1 className="text-xl font-bold mb-4">Analyse</h1>
           <form className='flex'>
             {ListAnalyses.map((category) => (
               <label key={category.category} className='flex flex-wrap mx-4'>
@@ -111,8 +140,8 @@ function AnalyseAndTraitement() {
           </div>
         </div>
 
-        <h1 className='text-xl font-bold mt-8 ml-12'>Traitement</h1>
         <div className='m-8 p-4 rounded-lg' style={{ boxShadow: '2px 2px 2px 2px rgba(0, 0, 0, 0.3)' }}>
+        <h1 className="text-xl font-bold mb-4">Traitement</h1>
         {rows.map((row, index) => (
             <form key={index} className='flex my-4'>
               <label className='mx-4'>
