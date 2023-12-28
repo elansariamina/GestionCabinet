690b77e512648fe08c52fc8ca329e422a6ef1272
kao19 <feiyukine@gmail.com>
2023-12-28 18:23:12 +0100

    display rapport and archive

---
+++ cabinet/src/components/Medecin/DocumentTable.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const DocumentTable = ({ documents, title, downloadHandler }) => {
+  return (
+    <div className="w-1/2 p-4">
+      <h2 className="text-xl font-bold mb-4">{title}:</h2>
+      {documents.length > 0 ? (
+        <table className="table-auto w-full text-#26a7cc">
+          <tbody>
+            {documents.map((document, index) => (
+              <tr key={index}>
+                <td className="text-center text-blue-950 font-comicSans">
+                  {new Date(document.date).toLocaleDateString()}
+                </td>
+                <td className="text-center">
+                  <button
+                    onClick={() => downloadHandler(document)}
+                    className="bg-#26a7cc hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded-full"
+                  >
+                    Télécharger
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Aucune {title.toLowerCase()} disponible.</p>
+      )}
+    </div>
+  );
+};
+
+export default DocumentTable;
